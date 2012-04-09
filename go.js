9fd4ec503e46dfc4bb8802bc3a154827116778e9
Christopher Toledo <simoon@gmx.com>
2012-04-09 10:57:49 +0400

    updated for VPS

--- go.js
+++ go.js
@@ -200,5 +200,4 @@ global.tweeted = false;
 
 setInterval(untilNext, 1000);
 
-app.listen(3001);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3001, '127.0.0.1');
